bdb546b129d88f326d7be8440046370db89901b1
owenwilliam <ouhai3698@163.com>
2020-04-04 15:59:59 +0800

    ttt

---
+++ src/test/java/org/owen/bookpub/BookTest.java
@@ -0,0 +1,12 @@
+package org.owen.bookpub;
+
+public class BookTest
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+        System.out.println(">>>>>>>");
+	}
+
+}
