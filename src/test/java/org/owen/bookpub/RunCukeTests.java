411f5b5bd3271a3a576b904f0cd04b839853260e
owenwilliam <ouhai3698@163.com>
2020-04-12 17:54:06 +0800

    v5.xx:Application Testing

--- src/test/java/org/owen/bookpub/RunCukeTests.java
+++ src/test/java/org/owen/bookpub/RunCukeTests.java
@@ -16,7 +16,7 @@ import cucumber.api.junit.Cucumber;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "html:build/reports/cucumber" }, glue = {
-		"cucumber.api.spring", "classpath:org.test.bookpub" }, monochrome = true)
+		"cucumber.api.spring", "classpath:org.owen.bookpub" }, monochrome = true)
 public class RunCukeTests
 {
 }
