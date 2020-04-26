e1244b7f5eed402ef9da9cb432dfede3faafb7dc
owenwilliam <ouhai3698@163.com>
2020-04-26 21:38:55 +0800

    test

---
+++ src/main/java/org/owen/bookpub/entity/CopyOfIsbn.java
@@ -0,0 +1,22 @@
+package org.owen.bookpub.entity;
+
+/**
+ * 自定义类型，名为Isbn
+ * @author OwenWilliam
+ * @date 2020/04/02
+ *
+ */
+public class CopyOfIsbn
+{
+	private String isbn;
+
+	public CopyOfIsbn(String isbn)
+	{
+		this.isbn = isbn;
+	}
+
+	public String getIsbn()
+	{
+		return isbn;
+	}
+}
